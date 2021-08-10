aaa22a23293ddfaa1bd687e20082e40c920ed8b6
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-10 13:47:36 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,27 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Good morning, Paul.",
+        options:["Good morning, Ann.", "And you?"],
+        answer: 0
+    },
+    {
+        q:"How are you?",
+        options:["Thank you.", "I'm fine, thanks."],
+        answer: 1
+    },
+    {
+        q:"Goodbye, Tony.",
+        options:["Goodbye, Sally. See you later.", "How are you?"],
+        answer: 0
+    },
+    {
+        q:"See you later!",
+        options:["Fine, thanks.", "See you."],
+        answer: 1
+    }
+
+
+
+]
